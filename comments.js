27176a8b0948948d5076cade0922ee3448e8cedf
laskovya <97305024+laskovya@users.noreply.github.com>
2023-08-07 02:25:18 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,15 +1,63 @@
 // create web server
-var express = require('express');
-var router = express.Router();
-var Comment = require('../models/comment');
-var User = require('../models/user');
-var Post = require('../models/post');
-var jwt = require('jsonwebtoken');
-var config = require('../config/database');
+// get comments from db
+// send comments to client
 
-// create comment
-router.post('/create', function(req, res){
-    var token = req.headers['authorization'];
-    var decoded = jwt.decode(token);
-    var comment = new Comment({
-        content: req.body.content,
+// import modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// create express app
+const app = express();
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost:27017/commentDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// create schema
+const commentSchema = new mongoose.Schema({
+    name: String,
+    comment: String
+});
+
+// create model
+const Comment = mongoose.model('Comment', commentSchema);
+
+// set view engine
+app.set('view engine', 'ejs');
+
+// use body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// set static folder
+app.use(express.static('public'));
+
+// get comments from db
+app.get('/', (req, res) => {
+    Comment.find({}, (err, comments) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('index', { comments: comments });
+        }
+    })
+});
+
+// post comments to db
+app.post('/', (req, res) => {
+    const comment = new Comment({
+        name: req.body.name,
+        comment: req.body.comment
+    });
+    comment.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
+// listen on port 3000
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
